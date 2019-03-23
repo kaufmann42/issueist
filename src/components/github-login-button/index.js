8e6499ad0872455d05a4e5dcda64e263cdc44ac1
kaufmann <kaufmann43@gmail.com>
2019-03-23 02:29:38 -0500

    added credits

--- src/components/github-login-button/index.js
+++ src/components/github-login-button/index.js
@@ -1,3 +1,8 @@
+/**
+ * Shamelessly stolen for modification from
+ * @see https://github.com/checkr/react-github-login
+ * @author checkr
+ */
 import GitHubLogin from './GitHubLogin';
 
 export default GitHubLogin;
