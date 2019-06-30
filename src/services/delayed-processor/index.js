a085fd0ef51a19a607db9a53e452e0c7f75b4c98
Zach Schultz <zacharymschultz@gmail.com>
2019-06-30 17:44:31 -0500

    fixing async forEach issue, replacing setInterval with continuous setTimeout calls

--- src/services/delayed-processor/index.js
+++ src/services/delayed-processor/index.js
@@ -1,4 +1,4 @@
-import  uuidv4  from 'uuid/v4';
+import uuidv4 from 'uuid/v4';
 import {store, retrieve} from '../storage';
 
 let existingPollingId;
@@ -6,11 +6,10 @@ let existingPollingId;
 /**
  * Stores an issue to be published at a later date.
  */
-export const delayIssue = async (newIssue) => {
-  let delayedIssues = await retrieve('delayedIssues') || []
+export const delayIssue = async newIssue => {
+  let delayedIssues = (await retrieve('delayedIssues')) || [];
   await store('delayedIssues', [...delayedIssues, {id: uuidv4(), ...newIssue}]);
 };
-
 
 /**
  * Continuously polls the storage for delayed issues.
@@ -18,21 +17,50 @@ export const delayIssue = async (newIssue) => {
  * or not the issue should be published.
  * Upon successfully publishing an issue, it is removed from storage.
  */
-export const startPollingDelayedIssues = (gh) => {
+export const startPollingDelayedIssues = gh => {
   if (existingPollingId) {
     window.clearInterval(existingPollingId);
   }
 
-  existingPollingId = window.setInterval(async () => {
-    console.log('Checking for delayed issues');
-    let delayedIssues = await retrieve('delayedIssues');
+  existingPollingId = window.setTimeout(
+    async () => await pollPublishAndRemove(gh),
+    5000,
+  );
+};
 
-    let idsToRemove = [];
-    delayedIssues.forEach(async issue => {
+/**
+ * Runs the actual logic for finding delayed issues, publishing them,
+ * and then removing them. Additionally creates a new setTimeout
+ * so that new delayed issues will be polled.
+ */
+async function pollPublishAndRemove(gh) {
+  let delayedIssues = await retrieve('delayedIssues');
+  let idsToRemove = await publishDelayedIssues(delayedIssues, gh);
+
+  idsToRemove.forEach(id => {
+    delayedIssues = delayedIssues.filter(issue => issue.id !== id);
+  });
+
+  await store('delayedIssues', delayedIssues);
+
+  existingPollingId = window.setTimeout(
+    async () => await pollPublishAndRemove(gh),
+    5000,
+  );
+}
+
+/**
+ * Publishes an array of issues to GitHub, then returns an array
+ * with their ids so they can be removed.
+ */
+async function publishDelayedIssues(delayedIssues, gh) {
+  let idsToRemove = [];
+  await Promise.all(
+    delayedIssues.map(async issue => {
       if (Date.now() > new Date(issue.date).getTime()) {
         const {id, user, repo, title, body} = issue;
 
-        const ghIssues = gh.getIssues(user, repo)
+        const ghIssues = gh.getIssues(user, repo);
         await ghIssues.createIssue({
           title,
           body,
@@ -41,19 +69,13 @@ export const startPollingDelayedIssues = (gh) => {
         // Issue successfully published, now set it for removal
         idsToRemove.push(id);
       }
-    });
-
-    idsToRemove.forEach(id => {
-      delayedIssues = delayedIssues.filter(issue => issue.id !== id)
-    })
-
-    await store('delayedIssues', delayedIssues);
-  }, 10000);
+    }),
+  );
+  return idsToRemove;
 }
 
 export const stopPollingDelayedIssues = () => {
   if (existingPollingId) {
-    window.clearInterval(existingPollingId)
+    window.clearInterval(existingPollingId);
   }
-}
-
+};
